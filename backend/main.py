c54bf28c51b2ad1afaa3bcf11803de5d3a0713f5
luizuk <luizuksouza@gmail.com>
2025-02-05 16:41:02 -0300

    fix: add cors policy

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,19 @@
 from fastapi import FastAPI
 from backend.db.database import init_db
 from backend.routes import router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+# Add CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],  # Allow your frontend origin
+    allow_credentials=True,
+    allow_methods=["*"],  # Allow all methods (GET, POST, etc.)
+    allow_headers=["*"],  # Allow all headers
+)
+
 
 @app.on_event("startup")
 def on_startup():
